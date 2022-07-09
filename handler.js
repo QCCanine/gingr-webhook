8fe6795b77ac2ee4f8a204e41ad09a162841d307
Zak Armistead <zakarmistead@gmail.com>
2022-07-09 14:03:22 -0400

    added concole.log to handler

--- handler.js
+++ handler.js
@@ -8,6 +8,8 @@ const service = new AirtableService()
 module.exports.handleEvent = async (event) => {
   const webhookEvent = JSON.parse(event.body);
 
+  console.log(webhookEvent)
+  
   // TODO: Validate event signature
   switch (webhookEvent["webhook_type"]) {
     case "check_in":
