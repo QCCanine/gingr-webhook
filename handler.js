751ea44bf6d7141c81bcb017527657f4fb19fb4e
David Illing <dilling@redventures.com>
2022-07-10 21:32:41 -0400

    validate webhook signature

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var crypto = require('crypto');
 const { AirtableService } = require('./airtableService');
 const { NotFoundError } = require('./errors/NotFoundError');
 
@@ -7,6 +8,13 @@ const service = new AirtableService()
 
 module.exports.handleEvent = async (event) => {
   const webhookEvent = JSON.parse(event.body);
+
+  if (!hasValidSignature(webhookEvent)) {
+    return {
+      statusCode: 403,
+      body: "Invalid signature"
+    }
+  }
   
   // TODO: Validate event signature
   switch (webhookEvent["webhook_type"]) {
@@ -18,6 +26,17 @@ module.exports.handleEvent = async (event) => {
       return { statusCode: 200 }
   }
 };
+
+function hasValidSignature(event) {
+  const token = `${event["webhook_type"]}${event["entity_id"]}${event["entity_type"]}`;
+  const signature = 
+    crypto
+      .createHmac('sha256', process.env.WEBHOOK_SIGNATURE_KEY)
+      .update(token)
+      .digest('hex');
+
+  return signature === event["signature"]
+}
 
 async function checkIn(event) {
   const data = event["entity_data"]
